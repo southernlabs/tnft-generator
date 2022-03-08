18d7961b6082e2f6b7d1529a454b83fec687713e
Alex <alexbs.ga@gmail.com>
2022-03-08 15:58:02 +0300

    Continue playground with TIP4.1

--- true-nft/components/tip4.1/Collection.sol
+++ true-nft/components/tip4.1/Collection.sol
@@ -1,4 +1,4 @@
-pragma ton-solidity >=0.43.0;
+pragma ton-solidity >=0.55.0;
 
 interface TIP4NftCollection {
     function name() public view responsible returns (string);
@@ -6,3 +6,10 @@ interface TIP4NftCollection {
     function maxSupply() public view responsible returns (uint128);
     function nftCode() public view responsible returns (TvmCell);
 }
+
+event nftCreated(
+    uint256 id,           // nft token id
+    address addrNft,      // nft token address
+    address addrOwner,    // nft token owner
+    address addrCreator   // initiator of creating the nft token
+);
--- true-nft/components/tip4.1/NFT.sol
+++ true-nft/components/tip4.1/NFT.sol
@@ -1,4 +1,4 @@
-pragma ton-solidity >=0.43.0;
+pragma ton-solidity >=0.55.0;
 
 interface TIP4Nft {
     function getNftInfo() external responsible returns(uint128 id, address addrOwner, address addrManager, address addrCollection);
@@ -6,3 +6,39 @@ interface TIP4Nft {
     function setManager(address addrManager, address addrSendGasTo, mapping(address => CallbackParams) callbacks);
     function getNftJsonContent() external responsible returns(string json);
 }
+
+struct CallbackParams {
+    uint256 value;      // ever value will send to address
+    TvmCell payload;
+}
+
+interface ITokenTransferCallback {
+    function tokenTransferCallback(
+        uint256 id,
+        address addrOldOwner,
+        address addrOldManager,
+        address addrNewOwner,
+        address addrCollection,
+        address addrSendGasTo,
+        TvmCell payload
+    ) external;
+}
+
+interface ISetManagerCallback {
+    function setManagerCallback(
+        uint256 id,
+        address addrOwner,
+        address addrOldManager,
+        address addrNewManager,
+        address addrCollection,
+        address addrSendGasTo,
+        TvmCell payload
+    ) external;
+}
+
+event TokenWasMinted(address owner);
+event OwnershipTransferred(address oldOwner, address newOwner);
+event ManagerChanged(address oldManager, address newManager);
+
+
+
